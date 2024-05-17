f5e7e846ef3164fcbdd4b7e886f8c99d946183ca
Axel Janssen <janssen.axel@gmail.com>
2024-05-17 20:55:47 +0200

    Updates to D39V (v1.0.26)

---
+++ plugins/FoldEnriched.js
@@ -0,0 +1,17 @@
+// FoldEnriched.js
+;(function () {
+  class MyPlugin {
+    install() {}
+    configure(pluginManager) {
+      pluginManager.jexl.addFunction('foldenriched', feature => {
+       return "Fold Enriched: " + feature;
+      })
+    }
+  }
+
+  // the plugin will be included in both the main thread and web worker, so
+  // install plugin to either window or self (webworker global scope)
+  ;(typeof self !== 'undefined' ? self : window).JBrowsePluginFoldEnriched = {
+    default: MyPlugin,
+  }
+})()
